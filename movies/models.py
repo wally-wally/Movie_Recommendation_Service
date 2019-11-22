5e69f5cd138db010bab9d6ac5077ea9d63a1e75f
wally-wally <wallys0213@gmail.com>
2019-11-22 15:19:16 +0900

    191122 | update modeling

---
+++ movies/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=150)
+    director = models.CharField(max_length=150)
+    user_rating = models.FloatField(
+        validators = [MinValueValidator(0.0), MaxValueValidator(10.0)]
+    )
+    poster_url = models.CharField(max_length=1000)
+    description = models.TextField()
+    genre_id = models.ManyToManyField(Genre, related_name='genre_movies', blank=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies', blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('-pk',)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=150)
+    score = models.IntegerField(
+        validators = [MinValueValidator(0), MaxValueValidator(10)]
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = ('-pk',)
