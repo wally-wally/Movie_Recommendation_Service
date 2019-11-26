031833e61b64197e97d0ccf7b49a9d496177e6cf
edugieun <gieun625@gmail.com>
2019-11-26 23:54:40 +0900

    191126 | design_accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,15 +2,16 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
 
+from django.utils.safestring import mark_safe
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'first_name', 'last_name',)
 
-
 class CustomUserCreationForm(UserCreationForm):
     CHOICES = [('M', '남'), ('W', '여')]
-    sex = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES, required=True)
+    sex = forms.ChoiceField(choices=CHOICES, required=True)
     age = forms.IntegerField(min_value=0, max_value=100, required=True)
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
